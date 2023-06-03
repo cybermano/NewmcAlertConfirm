1bbe488efa28e3e488315d4a31e66c2600983362
cybermano <mano.newmc@gmail.com>
2023-06-03 11:56:40 +0200

    Repo sync with home

--- NewmcAlertConfirm.js
+++ NewmcAlertConfirm.js
@@ -1,63 +1,3 @@
-/*
-2022 11 15 Work in progress
-2022 11 14 BETA VERSION V2
-2022 11 13 ALPHA VERSION V1
-
-AUTHOR: NewMediaConsulting - Mauro Romano
-
-Plugin to create bootstrap modal structure for custom alert/confirn dialogs
-JS Object OPTIONAL parameters to pass
-{
-    'containerID'   : 'NewmcAlertConfirm',              // id | default (must refer to html placeholder)
-    'dialog'        : 'modal',                          // string, variant 'alert'
-    'type'          : 'warning',                        // string, variant: 'info', 'danger', 'success'    
-    'color'         : HEXVAL,                           // defaults #222222 ( other values depends by type )
-    'bgcolor'       : HEXVAL,                           // default white ( other values depends by type ),    
-    'eID'           : elementID,                        // integer by DOM element
-    'checked'       : true|false,                       // boolean
-    'icon'          : 'fa-exclamation-triangle',        // FontAwesome icon class (icon class and optional 1 transformation or 1 motion, fa fa-2x exist)   
-    'title'         : 'Attenzione!',                    // Header title (could be populated with ajax return message)
-    'body'          : 'Voucher non ancora prenotato.',  // Explanation (could be populated with ajax return message)
-    'action'        : 'Vuoi riscattarlo comunque?',     // Action to take (could be populated with ajax return message)
-    'href'          : URL                               // to load with window.location.href (usually the page httpUrl for redirect without POST/GET data)
-    'hrefDelay'     : millisecond                       // for SetTimeout funciton of href redirect
-    'returnFalse'   : false                             // return false    
-    'triggerON'     : true                              // trigger buttons click
-    'cancelText'    : true                              // Text for Cancelbutton
-    'okText'        : true                              // Text for OKButton
-}
-
-
-DECLARATION AND USAGE
-
-IN PAGE LOAD ON TOP
-CSS STYLE FOR CUSTOMIZATION
-<link rel="stylesheet" href="/site/templates/scripts/newmc/NewmcAlertConfirm.css">
-
-IN PAGE LOAD AT BOTTOM OF HTML
-PLACEHOLDER (if "id" will be different from "NewmcAlertConfirm" it must be declared into object key:value pair as containerID:'YOUR_PLACEHOLDER_ID' )
-<div id="NewmcAlertConfirm" class="newmc-dialog-plugin"></div>
-
-AT SCRIPT BEGIN
-LOAD CONSTRUCTOR (THIS FILE)
-<script src="/site/templates/scripts/newmc/NewmcAlertConfirm.js"></script>
-
-BASIC USAGE IN PAGE SCRIPT
-CALL A NEW CONSTRUCTOR AND PASS TO IT YOUR PARAMETERS (CALL EACH TIME YOU WANT, NO DESTROY NEEDS)
-// // CUSTOM ALERT
-    var customAlertConfirm = new NewmcAlertConfirm({
-        dialog:'alert',
-        type: 'danger',
-        title: 'Error!',
-        body: 'Oooops... Something went wrong',
-        icon: 'fa-ban',
-    });
-// // END CUSTOM ALERT
-
-For more customizations, see defaultOptions
-
-*/
-
 class NewmcAlertConfirm {
     constructor(options) {
 
@@ -97,6 +37,7 @@ class NewmcAlertConfirm {
             'action'        : 'Perform NewMC Custom Action?',   // Could be populated with an ajax return text
             'href'          : null,                             // URL for window.reload.href (redirect)
             'hrefDelay'     : 0,                                // milliseconds
+            'timeOutHide'   : null,                             // milliseconds
             'returnFalse'   : false,                            // return false
             'triggerON'     : true,                             // trigger buttons click
             'cancelText'    : 'Cancel',                         // Default Text for Cancelbutton
@@ -155,6 +96,17 @@ class NewmcAlertConfirm {
 
                 }); 
             }
+
+            if (options.timeOutHide){
+                setTimeout(function() {
+                    $('#custom-' + options.dialog).modal('hide');
+                    $('.modal-backdrop').remove();
+                    if (options.href != null){
+                        window.location.href = options.href;
+                    }                    
+                }, options.timeOutHide);                
+            }
+
 
         };
 
